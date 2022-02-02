f3dff0cf43120dc90c7fae7a95451e51a7efac32
miska542 <2236707671@qq.com>
2022-02-02 10:18:52 +0800

    new

--- src/new1/longestSubstringWithAtLeastKRepeatingCharacters.java
+++ src/new1/longestSubstringWithAtLeastKRepeatingCharacters.java
@@ -1,5 +1,8 @@
 package new1;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class longestSubstringWithAtLeastKRepeatingCharacters {
     public int longestSub(String s,int k){
         return dfs(s,0,s.length()-1,k);
@@ -32,6 +35,29 @@ public class longestSubstringWithAtLeastKRepeatingCharacters {
             res=Math.max(res,dfs(s,start,i-1,k));
         }
         return res;
+    }
+    public int longest(String s,int k){
+        int ans=0,n=s.length();
+        char[] cs=s.toCharArray();
+        int[] dir =new int[26];
+        for (int p=1;p<27;p++){
+            Arrays.fill(dir,0);
+            for (int i=0,j=0,t=0,sum=0;i<n;i++){
+
+                int u=cs[i]-'a';
+                dir[u]+=1;
+                if (dir[u]==1) t+=1;
+                if (dir[u]==k) sum+=1;
+                while (t>p){
+                    int c=cs[j++]-'a';
+                    dir[c]-=1;
+                    if (dir[c]==0) t-=1;
+                    if (dir[c]==k-1) sum-=1;
+                }
+                if (t==sum) ans=Math.max(ans,i-j+1);
+            }
+        }
+        return ans;
     }
 
 }
