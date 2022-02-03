65dfc3a014d73fedefb9daf8ab12d0f33e52a167
miska542 <2236707671@qq.com>
2022-02-03 17:13:14 +0800

    new

--- src/new1/Gupiaok.java
+++ src/new1/Gupiaok.java
@@ -1,32 +1,37 @@
 package new1;
 
 public class Gupiaok {
-	public static int getnum(int k,int[] price) {
-		int n=price.length;
-		int[][][] dp= new int[n+1][k+1][2];
-		for(int i=0;i<=n;i++) {
-			for(int k1=k;k1>=1;k1--) {
-				if(i==0) {
+	public static int getnum(int maxk,int[] prices) {
+		int n=prices.length;
+		if(n==0) return 0;
+		else if(maxk>n/2) return getMaxx(prices);
+		else return getMax(maxk,prices);
+	}
+	public static int getMax(int maxk, int[] prices){
+		int n=prices.length;
+		int[][][] dp=new int[n][maxk+1][2];
+		for(int k=0;k<=maxk;k++){
+			dp[0][k][0]=0;
+			dp[0][k][1]=-prices[0];
+		}
+		for(int i=1;i<n;i++){
+			for(int k=maxk;k>0;k--){
 
-			
-					dp[i][k][0]=0;
-					dp[i][k][1]=Integer.MIN_VALUE;
-					
-				continue;
-				}
-				dp[i][k][0]=Math.max(dp[i-1][k][0], dp[i-1][k][1]+price[i-1]);
-				System.out.print("i"+i+"\n"+dp[i][k][0]+"\n");
-				dp[i][k][1]=Math.max(dp[i-1][k][1], dp[i-1][k-1][0]-price[i-1]);
-				System.out.print(dp[i][k][1]+"\n");
+				dp[i][k][0]=Math.max(dp[i-1][k][0],dp[i-1][k][1]+prices[i]);
+				dp[i][k][1]=Math.max(dp[i-1][k][1],dp[i-1][k-1][0]-prices[i]);
 			}
 		}
-		return dp[n-1][k][0];
+		return dp[n-1][maxk][0];
 	}
-	public static void main(String[] wa) {
-		int k=2;
-		int[] price={1,3,5,10,50,3,1,20,1,50,60,20,50,1,90};
-		System.out.print(getnum(k, price));
-		
+	public static int getMaxx(int[] prices){
+		int dpi0=0,dpi1=Integer.MIN_VALUE;
+		for(int price:prices){
+			int tmp=dpi0;
+			dpi0=Math.max(dpi0,dpi1+price);
+			dpi1=Math.max(dpi1,tmp-price);
+		}
+		return dpi0;
+
 	}
 
 }
